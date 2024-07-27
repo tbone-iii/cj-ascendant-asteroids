3dc25f1e8e5414851ffaf90d4f408c8922829277
Ethanscharlie <ethanscharlie@protonmail.com>
2024-07-27 15:01:46 -0600

    Almost working get user score command

--- article_overload/cogs/scoreboard.py
+++ article_overload/cogs/scoreboard.py
@@ -3,12 +3,14 @@ from typing import Any
 from discord import (
     Embed,
     Interaction,
+    User,
     app_commands,
 )
 from discord.ext import commands
 from utils.constants import SCORE_BOARD_SPACING_SIZE
 
 from article_overload.bot import ArticleOverloadBot
+from article_overload.db.objects import Score
 
 InteractionChannel = Any
 
@@ -24,6 +26,16 @@ class ScoreboardCog(commands.Cog):
         """
         self.client = client
         self.database_handler = client.database_handler
+
+    async def get_score_text_row(self, score_object: Score) -> str:
+        """Get a score in a text row."""
+        username = (await self.client.fetch_user(score_object.user_id)).name.strip()
+
+        return (
+            f"`{username:<{SCORE_BOARD_SPACING_SIZE}}"
+            f"{score_object.score!s:<{SCORE_BOARD_SPACING_SIZE}}"
+            f"{score_object.latest_played.strftime("%B %d, %Y"):<{SCORE_BOARD_SPACING_SIZE}}`\n"
+        )
 
     @app_commands.command(name="show_scoreboard", description="Shows the scores of the top ten players.")
     async def show_scoreboard(self, interaction: Interaction) -> None:
@@ -43,15 +55,24 @@ class ScoreboardCog(commands.Cog):
 
         player_scores = await self.client.database_handler.get_top_n_scores(10)
         for score_object in player_scores:
-            username = (await self.client.fetch_user(score_object.user_id)).name.strip()
-            score_text += (
-                f"`{username:<{SCORE_BOARD_SPACING_SIZE}}"
-                f"{score_object.score!s:<{SCORE_BOARD_SPACING_SIZE}}"
-                f"{score_object.latest_played.strftime("%B %d, %Y"):<{SCORE_BOARD_SPACING_SIZE}}`\n"
-            )
+            score_text += await self.get_score_text_row(score_object)
 
         embed.add_field(name=score_text, value="")
 
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="get_user_score", description="Shows the scores of the top ten players.")
+    async def get_user_score(self, interaction: Interaction, mention_target: User) -> None:
+        """Bot command.
+
+        Description: Shows the score of a player.
+        :Return: None
+        """
+        # TODO: use get_player_score_object instead of score
+        score_object = await self.client.database_handler.get_player_score(mention_target.id)
+
+        embed = Embed(title="Scoreboard")
+        embed.add_field(name=await self.get_score_text_row(score_object), value="")
         await interaction.response.send_message(embed=embed)
 
 
